46e9912fc77a295c2d17651894e180068dbd1d8e
李宁 <lining01@youxin.com>
2018-11-15 19:05:13 +0800

    _884提交

--- src/_232/Client.java
+++ src/_232/Client.java
@@ -7,7 +7,21 @@ package _232;
 public class Client {
 
     public static void main(String[] args) {
-        Solution1 solution1 = new Solution1();
-        System.out.println(solution1.isEquls("a##c","#a#c"));
+        Solution1<String> solution1 = new Solution1();
+        solution1.push("A");
+        solution1.push("B");
+        solution1.push("C");
+
+
+
+
+        solution1.print();
+        solution1.printAndPop();
+        solution1.print();
+        solution1.printAndPop();
+        solution1.printAndPop();
+
+
+
     }
 }
--- src/_232/Solution1.java
+++ src/_232/Solution1.java
@@ -4,36 +4,45 @@ import java.util.Stack;
 
 /**
  * @author lining
- * @date 18-11-15
- * @实现思路：例用stack的特性来处理
+ * @date 18-11-15 @实现思路：
  */
-public class Solution1 {
-    public boolean isEquls(String str1, String str2) {
-        String stack = backSpace(str1);
-        String stack2 = backSpace(str2);
-        if (stack.equals(stack2)) {
-            return true;
-        }
-        return false;
+public class Solution1<E> {
 
+    private Stack<E> stack = new Stack();
+    private Stack<E> stack2 = new Stack();
+
+    void push(E e) {
+        stack.add(e);
     }
 
-    private String backSpace(String str) {
-        Stack<Character> stack = new Stack<>();
-        if (str != null) {
-            for (Character character : str.toCharArray()) {
-
-                if ('#' == character) {
-                    if (stack.isEmpty()) {
-                        continue;
-                    }
-                    stack.pop();
-                } else {
-                    stack.push(character);
-                }
+    E pop() {
+        if (stack2.empty()) {
+            while (!stack.empty()) {
+                stack2.push(stack.pop());
             }
         }
-        return stack.toString();
+        return stack2.pop();
+    }
+
+    E peek() {
+        if (stack2.empty()) {
+            while (!stack.empty()) {
+                stack2.push(stack.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    boolean empty() {
+        return stack2.empty() && stack.empty();
+    }
+
+    void printAndPop() {
+        System.out.println(this.pop());
+    }
+
+    void print() {
+        System.out.println(this.peek());
     }
 
 }
